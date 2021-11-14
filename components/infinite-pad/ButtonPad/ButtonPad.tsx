18ce5815e4b272fcaf08226fe62afd70eb1f5eaa
Jônatas Santos <jonatas.santos@zapier.com>
2021-11-13 21:13:25 -0300

    Play notes

--- components/infinite-pad/ButtonPad/ButtonPad.tsx
+++ components/infinite-pad/ButtonPad/ButtonPad.tsx
@@ -8,7 +8,7 @@ export const ButtonPad = ({ label = 'A', active = false, ...props }) => {
   return (<div className="justify-self-center">
       <label className={s.label}>{label}</label>
       <button className={[s.button, s['button-active']].join(' ')}
-              onClick={() => props.setState({ currentKey: label })}>
+              onClick={() => props.onToggle({ key: label })}>
         <span className={s.led + (active ? ' ' + s['led-active'] : '')}></span>
       </button>
     </div>
--- components/infinite-pad/InfinitePadSynth/InfinitePadSynth.tsx
+++ components/infinite-pad/InfinitePadSynth/InfinitePadSynth.tsx
@@ -1,22 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ButtonPad } from '@components/infinite-pad'
 import s from './InfinitePadSynth.module.css'
 import ScreenDisplay from '../ScreenDisplay'
 import PatchButton from '../PatchButton'
 import Knob from '@components/infinite-pad/Knob'
-
+import { Howl, Howler } from 'howler'
 
 const keys = [
   'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
 ]
 
+const patches = [
+  'Abaddon Choir',
+  'Lunaris 1'
+]
+
+const translateNote = (code: string) => {
+  return code.replace('#', 'sharp')
+}
+
+const generateSoundsForPatch = (patchName: string) =>
+  Object.fromEntries(keys.map(k => [k, new Howl({
+    src: [`/sounds/Infinite Pad - ${patchName} - ${translateNote(k)}.mp3`],
+    loop: true,
+    sprite: {
+      main: [0, 10000, true],
+      main2: [1000, 9000, true]
+    }
+  })]))
+
+
 const InfinitePadSynth = () => {
-  const [state, setState] = useState({
-    currentKey: null
+  const [state, setState] = useState<{ currentKey: string | null, prevKey: string | null, isPlaying: boolean }>({
+    currentKey: null,
+    prevKey: null,
+    isPlaying: false
   })
+
+
+  const [sounds, setSounds] = useState(generateSoundsForPatch('Abaddon Choir'))
 
   const [cutoff, setCutoff] = useState(127)
   const [volume, setVolume] = useState(127)
+
+  const onButtonPadToggle = ({ key }: { key: string }) => {
+    setState(prev => {
+      if (prev.isPlaying && prev.currentKey) {
+        let playingSound: Howl = sounds[prev.currentKey]
+        playingSound.fade(1, 0, 5000)
+        setTimeout(() => {
+          playingSound.stop()
+        }, 5000)
+      }
+      return { ...prev, prevKey: prev.currentKey, currentKey: key, isPlaying: true }
+    })
+  }
+
+  useEffect(() => {
+    if (state.isPlaying && state.currentKey) {
+      state.currentKey && sounds[state.currentKey].fade(0, 1, 1000)
+      state.currentKey && sounds[state.currentKey].play('main')
+    }
+  }, [state.currentKey, state.isPlaying])
 
   return (
     <div className={s.synth}>
@@ -34,7 +79,7 @@ const InfinitePadSynth = () => {
           <Knob label='Volume' initialValue={volume} setValue={(v: number) => setVolume(v)} />
         </div>
         {keys.map(k => (
-          <ButtonPad key={k} label={k} active={state?.currentKey == k} setState={setState} />
+          <ButtonPad key={k} label={k} active={state?.currentKey == k} onToggle={onButtonPadToggle} />
         ))}
       </div>
     </div>
